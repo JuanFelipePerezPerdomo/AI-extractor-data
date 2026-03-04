5ccbffcdf370c015cd3c8aa2248c5dc1cb1afaf6
JuanFelipePerezPerdomo <juanfelipeperezperdomo@gmail.com>
2026-03-04 12:27:20 +0000

    refactorizacion

--- src/test.py
+++
@@ -1,8 +0,0 @@
-import sys
-try:
-    from google import genai
-    print(f"✅ ¡Éxito! Librería 'google-genai' detectada.")
-    print(f"🐍 Estás usando Python {sys.version.split()[0]}")
-    print(f"📂 Ubicación del entorno: {sys.prefix}")
-except ImportError:
-    print("❌ Error: No se encuentra la librería. ¿Activaste el entorno virtual?")
